589215fb275188cbadae92655b043875c1c1b051
MehdiRezaii <99mehdirezaii99@gmail.com>
2023-02-20 02:38:53 +0330

    Loading has been added to the post details page

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -6,6 +6,7 @@ import { useEffect } from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import { Container, Typography } from "@mui/material";
+import Loading from "@/components/Loading";
 export const getStaticProps: GetStaticProps = async (context) => {
 
     const id = context.params?.id as string;
@@ -38,17 +39,15 @@ function Post() {
     const id = route.query.id
     const { data, isLoading } = useQuery(['singlePost', id], () => fetchPost(id));
 
-    if (isLoading) return "Loading ..."
-
     return (
         <Container className='mt-10'>
-            <Typography className="my-5" component="div">
+            {isLoading ? <Loading /> : <><Typography className="my-5" component="div">
                 <Link className="bg-stone-800 shadow my-40 p-3 rounded-lg" shallow={true} href='/'>Back</Link>
             </Typography>
-            <p>title : {data.title}</p>
-            <p>id : {data.id}</p>
-            <p>body : {data.body}</p>
-            <p>userId : {data.userId}</p>
+                <p>title : {data.title}</p>
+                <p>id : {data.id}</p>
+                <p>body : {data.body}</p>
+                <p>userId : {data.userId}</p></>}
         </Container>
     )
 
