f7d6eab39a62aae75d11cae6047e40e7c60ece3a
Mehdi Rezaii <99mehdirezaii99@gmail.com>
2023-02-21 13:03:11 +0330

    Added ErrorBoundary

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import Navbar from '@/components/Navbar'
 import { QueryClient, QueryClientProvider, Hydrate } from 'react-query';
 import { useState } from 'react';
 import { configQueryClient } from '@/config/configQueryClient';
+import ErrorBoundary from '@/components/ErrorBoundary';
 
 
 
@@ -11,11 +12,13 @@ import { configQueryClient } from '@/config/configQueryClient';
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient(configQueryClient))
   return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <Navbar />
-        <Component {...pageProps} />
-      </Hydrate>
-    </QueryClientProvider>
+    <ErrorBoundary>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Navbar />
+          <Component {...pageProps} />
+        </Hydrate>
+      </QueryClientProvider>
+    </ErrorBoundary>
   )
 }
---
+++ store/useSearchValue.ts
@@ -0,0 +1,23 @@
+import { Post } from '@/types/post'
+import { searchPost } from '@/utils/searchPost'
+import { create } from 'zustand'
+import SearchValueState from '@/types/SearchValueState'
+
+const useSearchStore = create<SearchValueState>((set) => ({
+    value: [],
+    fillter: (data) => {
+        console.log('filtter run setter',data)
+        set(() => ({ value: data }))
+    },
+    setAllPost: (data) => {
+        console.log('setAllPost run setter',data)
+        set(() => ({ value: data }))
+    },
+    reverseList:(data) => {
+        set(() => {
+            return({value: [...data]})
+        })
+    }
+}))
+
+export default useSearchStore;
